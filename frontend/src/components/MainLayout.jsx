45b9df419b6a6badbb7e53abf6fdcd6151fc2f1c
Kapil <kapilishwarkar2004@gmail.com>
2026-02-25 17:03:50 +0530

    fix: correct import case for LeftSideBar in MainLayout.jsx

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import LeftSidebar from './LeftSideBar'
+import LeftSidebar from './LeftSidebar'
 
 const MainLayout = () => {
   return (
